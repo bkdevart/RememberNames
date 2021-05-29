7ac73435d385ff8f6643a9fe922d4439f77fdf53
Brandon Knox <bknox.dev@gmail.com>
2021-05-29 12:19:14 -0700

    Initial Commit

---
+++ RememberNames/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RememberNames
+//
+//  Created by Brandon Knox on 5/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
